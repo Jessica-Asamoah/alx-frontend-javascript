eb8a35be0d55b9c91388ed36e36c49e45d62da7f
Jessica Asamoah <jessicaasamoah903@gmail.com>
2024-03-13 15:02:16 +0000

    TypeScript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,29 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 23,
+  location: "Barcelona"
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 21,
+  location: "Texas"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+function renderTable() {
+  const table = document.createElement("table");
+  studentsList.forEach(student => {
+    const row = table.insertRow();
+    const firstNameCell = row.insertCell();
+    firstNameCell.innerHTML = student.first
